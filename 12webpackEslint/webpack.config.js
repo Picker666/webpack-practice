8498fce7c6c44b09a8063146de1234c5337ff14a
Picker666 <1034790395@qq.com>
2021-10-16 11:24:03 +0800

    hot update for html

--- 12webpackEslint/webpack.config.js
+++ 12webpackEslint/webpack.config.js
@@ -1,22 +1,30 @@
-const { resolve } = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
-const ESLintWebpackPlugin = require("eslint-webpack-plugin");
+const { resolve } = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: ['./src/index.js', './src/index.html'],
   output: {
-    filename: "built.js",
-    path: resolve(__dirname, "build"),
+    filename: 'built.js',
+    path: resolve(__dirname, 'build'),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
         exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          // 关闭es6  module
+          esModule: false,
+        },
       },
     ],
   },
@@ -24,28 +32,28 @@ module.exports = {
     alias: {
       // # ！！！！！！注意如果配置了此处 要安装 eslint-import-resolver-webpack的插件！！！！！！！！ #
       // 在package.json 配置eslinConfig 或者添加.eslintrc文件
-      "@": resolve(__dirname, "src"),
+      '@': resolve(__dirname, 'src'),
     },
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "./css/index.css",
+      filename: './css/index.css',
     }),
     new OptimizeCssAssetsWebpackPlugin(),
-    () => ["postcss-preset-env", {}],
+    () => ['postcss-preset-env', {}],
     new webpack.HotModuleReplacementPlugin({
       // Options...
     }),
     new ESLintWebpackPlugin({
       fix: true,
-      extensions: ["js", "json"],
-      exclude: "/node_modules/",
+      extensions: ['js', 'json'],
+      exclude: '/node_modules/',
     }),
   ],
-  mode: "development",
+  mode: 'development',
   devServer: {
     compress: true,
     port: 3001,
